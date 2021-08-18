ebeedceee2ef2ba421a9e820e7d20c3cb8a88fdb
Mahbubur Rahman <mrrohanc@gmail.com>
2021-08-18 19:47:46 +0600

    slice added

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ function getPin() {
 function generatePin() {
     const showGeneratePin = document.getElementById('show-generate-pin');
     showGeneratePin.value = getPin();
-    document.getElementById('tryCount').innerText = 3; 
+    document.getElementById('tryCount').innerText = 3;
+    const successMsg = document.getElementById('success-msg').style.display="none";
+    const failMsg = document.getElementById('fail-msg').style.display="none";
 }
 
 document.getElementById('key-pad').addEventListener('click', function (event) {
@@ -23,6 +25,9 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         if (key == 'C') {
             showKeyPadNumber.value = '';
         }
+        if (key == '<') {
+            showKeyPadNumber.value = currentKey.slice(0, currentKey.length-1);
+        }
     }
     else {
         showKeyPadNumber.value = currentKey + key;
@@ -30,20 +35,20 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
 });
 
 function verification() {
-    const typedNumber = document.getElementById('show-keyPad-number').value;
+    const typedNumber = document.getElementById('show-keyPad-number');
     const generatePin = document.getElementById('show-generate-pin').value;
     const successMsg = document.getElementById('success-msg');
     const failMsg = document.getElementById('fail-msg');
-    const tryAction = document.getElementById('try-action');
     const tryActionValue = document.getElementById('tryCount');
     const tryActionCount = parseInt(tryActionValue.innerText);
     if (tryActionCount == 0) {
         alert("Please Generate New Pin");
     }
     else {
-        if (generatePin == typedNumber) {
+        if (generatePin == typedNumber.value) {
             successMsg.style.display = 'block';
             failMsg.style.display = 'none';
+            typedNumber.value = '';
         }
         else {
             tryActionValue.innerText = tryActionCount-1;
